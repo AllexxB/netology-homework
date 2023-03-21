51f6a60310406cf14be704b6d1020b51956dd4f3
AlexPC <barvinko@mail.ru>
2023-03-21 17:03:31 +0300

    Pravka1

--- terraform-02/02/src/locals.tf
+++ terraform-02/02/src/locals.tf
@@ -1,5 +1,5 @@
 locals {
-    name_for_instance_web = var.vm_db_name
-    name_for_instance_db = var.vm_web_name
+    name_for_instance_web = "${var.vm_db_name}"
+    name_for_instance_db = "${var.vm_web_name}"
     
 }
